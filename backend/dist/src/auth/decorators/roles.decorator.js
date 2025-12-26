70f12aae75a6d5ee3ff34319d99e700fa518ac0d
Hrithik <hrithikbangwal1204@gmail.com>
2025-12-26 23:45:00 +0530

    chore(): remove dist from git tracking

--- backend/dist/src/auth/decorators/roles.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = Roles;
-function Roles() {
-    return () => {
-    };
-}
-//# sourceMappingURL=roles.decorator.js.map
--- backend/dist/src/auth/dto/refresh-token.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RefreshTokenDto = void 0;
-class RefreshTokenDto {
-}
-exports.RefreshTokenDto = RefreshTokenDto;
-//# sourceMappingURL=refresh-token.dto.js.map
